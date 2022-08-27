6350a92769cbdfe5c8962ba32c4ce156d6f561a5
StanLong <youlinbhz@sina.cn>
2022-08-27 16:55:28 +0800

    指针

--- HelloC++/Hello.cpp
+++ HelloC++/Hello.cpp
@@ -2,28 +2,25 @@
  
 using namespace std;
  
-// ä¸»å‡½æ•°
+// Ö÷º¯Êý
 int main(int argc, char *argv[])
 {	
-	int arr[] = {9, 5, 6, 8, 2, 7 ,3 ,4, 1};
-	int len = sizeof(arr) / sizeof(arr[0]);
-	
-	for(int i=0; i<len; i++)
-	{
-		int temp = 0;
-		for(int j=i; j<len; j++)
-		{
-			if(arr[i] > arr[j])
-			{
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-			}
-		}
-	}
+	int a = 10;
 
-	for(int i=0; i<len; i++)
-	{
-		printf("%d ", arr[i]);
-	}
+	// 1. Ö¸ÕëµÄ¶¨Òå
+	// Ö¸Õë¶¨ÒåµÄÓï·¨:  Êý¾Ý¹ËÑ×Îä * Ö¸Õë±äÁ¿Ãû
+	int * p;
+
+	//  &ÊÇÈ¡Ö··ûºÅ£¬ &a ±íÊ¾È¡µ½±äÁ¿aµÄÄÚ´æµØÖ·
+	p = &a;
+
+	// printf(&a);
+	// printf(p);
+	cout << "aµÄµØÖ·Îª: " << &a << endl; // aµÄµØÖ·Îª: 0x61fe44
+
+
+	// 2. Ö¸ÕëµÄÊ¹ÓÃ
+	// Ö¸ÕëÇ°¼Ó * ´ú±íÒýÓÃ£¬ ´ú±íÖ¸ÕëÖ¸ÏòÄÚ´æÖÐµÄÊý¾Ý
+	*p = 100;
+	cout << "a=" << a << endl; // a=100
 }
--- README.md
+++ README.md
@@ -3,3 +3,9 @@ C++ 从入门到入土
 
 VS 配置C++开发环境
 https://blog.csdn.net/qq_52392804/article/details/124779708
+
+
+
+VS Code运行C++ 控制台中文乱码问题解决
+
+https://blog.csdn.net/qq_45740349/article/details/109249254
