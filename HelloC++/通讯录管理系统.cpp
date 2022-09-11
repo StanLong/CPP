377a681c16a3efd65e8422d603d8eabe3fe10993
StanLong <youlinbhz@sina.cn>
2022-09-11 12:45:08 +0800

    内存模型

--- HelloC++/通讯录管理系统.cpp
+++ HelloC++/通讯录管理系统.cpp
@@ -1,77 +1,19 @@
 # include <iostream>
-#define MAX 1000 // 定义宏常量MAX表示通讯录最大人数
 using namespace std;
 
 
-// 显示菜单
-void showMenu()
+int* func()
 {
-    
-    cout << "*************************" <<endl;
-    cout << "***** 1、添加联系人 *****" <<endl;
-    cout << "***** 2、显示联系人 *****" <<endl;
-    cout << "***** 3、删除联系人 *****" <<endl;
-    cout << "***** 4、查找联系人 *****" <<endl;
-    cout << "***** 5、修改联系人 *****" <<endl;
-    cout << "***** 6、清空联系人 *****" <<endl;
-    cout << "***** 0、退出通讯录 *****" <<endl;
-    cout << "*************************" <<endl;
+    // 利用new 关键字，可以将数据开辟到堆区
 
+    int *p = new int (10);
+    return p;
 }
 
-// 联系人结构体
-struct Person
+int main(int argc, char *argv[])
 {
-    string m_name;
-    int m_sex;
-    int m_age;
-    string m_phone;
-    string m_addr;
-};
 
+   int * p = func();
 
-// 通讯录结构体
-struct AddressBooks
-{
-    Person personArray[MAX];
-    int m_size;
-};
-
-
-
-
-
-int main()
-{
-    int select = 0;
-    while(true)
-    {
-        showMenu();
-        cin >> select;
-        switch (select)
-        {
-        case 1 : // 添加联系人
-            break;
-        case 2 : // 显示联系人
-            break;
-        case 3 : // 删除联系人
-            break;
-        case 4 : // 查找联系人
-            break;
-        case 5 : // 修改联系人
-            break;
-        case 6 : // 清空联系人
-            break;
-        case 0 : // 退出通讯录
-            cout << "欢迎下次登录" << endl;
-            system("pause");
-            return 0;
-            break;
-        
-        default:
-            break;
-        }
-    }
-    
-    return 0;
+   cout << *p << endl;
 }
