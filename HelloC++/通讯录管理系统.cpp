54f0c207c2b2fccfe3fac1a14f044fc9887fa7f9
StanLong <youlinbhz@sina.cn>
2022-09-13 22:10:15 +0800

    C++核心编程

--- HelloC++/通讯录管理系统.cpp
+++ HelloC++/通讯录管理系统.cpp
@@ -1,19 +1,26 @@
 # include <iostream>
 using namespace std;
 
-
-int* func()
+void fun(int& ref)
 {
-    // 利用new 关键字，可以将数据开辟到堆区
-
-    int *p = new int (10);
-    return p;
+    ref = 100; // ref 是引用，转换为 * ref = 100
 }
 
 int main(int argc, char *argv[])
 {
+    int a = 10;
 
-   int * p = func();
+    int& ref = a; // 这行代码在内部自动转换为 int * const ref = &a; 指针常量是指针指向不可改，也说明了为什么引用不可改
+    ref = 20; // 内部发现 ref 是引用， 自动帮我们转换为 * ref = 20;
 
-   cout << *p << endl;
+    cout << "a=" << a << endl;
+    cout << "ref=" << ref << endl;
+
+    fun(a);
+    cout << "a=" << a << endl;
+    cout << "ref=" << ref << endl;
+
+    return 0;
+
+   
 }
