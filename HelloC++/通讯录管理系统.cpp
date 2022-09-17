6d619f74fbc7cab1fc2167afbdd36ba51e85cc5f
StanLong <youlinbhz@sina.cn>
2022-09-17 11:17:45 +0800

    面向对象

--- HelloC++/通讯录管理系统.cpp
+++ HelloC++/通讯录管理系统.cpp
@@ -1,26 +1,28 @@
 # include <iostream>
 using namespace std;
 
-void fun(int& ref)
+class Person
 {
-    ref = 100; // ref 是引用，转换为 * ref = 100
-}
+    // 构造函数
+    public:
+    Person()
+    {
+        cout << "构造函数被调用" << endl;
+    }
 
+    // 析构函数
+    ~ Person()
+    {
+        cout << "析构函数被调用" << endl;
+    }
+};
+
+void test()
+{
+    Person p;
+}
 int main(int argc, char *argv[])
 {
-    int a = 10;
-
-    int& ref = a; // 这行代码在内部自动转换为 int * const ref = &a; 指针常量是指针指向不可改，也说明了为什么引用不可改
-    ref = 20; // 内部发现 ref 是引用， 自动帮我们转换为 * ref = 20;
-
-    cout << "a=" << a << endl;
-    cout << "ref=" << ref << endl;
-
-    fun(a);
-    cout << "a=" << a << endl;
-    cout << "ref=" << ref << endl;
-
-    return 0;
-
-   
+    test();
 }
+
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 C++ 从入门到入土
 
 VS 配置C++开发环境
+
 https://blog.csdn.net/qq_52392804/article/details/124779708
 
 
