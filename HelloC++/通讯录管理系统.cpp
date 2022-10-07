70c3565276294a54b59a4bf0a69049cc6c80c904
StanLong <youlinbhz@sina.cn>
2022-10-07 13:59:00 +0800

    继承

--- HelloC++/通讯录管理系统.cpp
+++ HelloC++/通讯录管理系统.cpp
@@ -1,28 +1,61 @@
 # include <iostream>
 using namespace std;
 
-class Person
+class Base
 {
-    // 构造函数
     public:
-    Person()
-    {
-        cout << "构造函数被调用" << endl;
-    }
+    int m_A;
 
-    // 析构函数
-    ~ Person()
+    protected:
+    int m_B;
+
+    private:
+    int m_C;
+
+    public:
+    Base()
     {
-        cout << "析构函数被调用" << endl;
+        cout << "父类构造函数！" <<endl;
+    }
+    ~Base()
+    {
+        cout << "父类析构函数！" <<endl;
     }
 };
 
-void test()
+class Son:public Base
 {
-    Person p;
+    public:
+    int m_D;
+
+    public:
+    Son()
+    {
+        cout << "子类构造函数！" <<endl;
+    }
+    ~Son()
+    {
+        cout << "子类析构函数！" <<endl;
+    }
+};
+
+
+void test01()
+{
+    Son son; 
+    // 父类构造函数！
+    // 子类构造函数！
+    // 子类析构函数！
+    // 父类析构函数！
+
+    // 根据系统输出可知
+    // 继承中的构造的析构顺序如下
+    // 先构造父类，再构造子类。先析构子类，再析构父类
+
 }
+
 int main(int argc, char *argv[])
 {
-    test();
+    test01();
 }
 
