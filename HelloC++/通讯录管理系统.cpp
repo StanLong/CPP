5cd8e9ec1ae521af93efb813d52c7d4fe00f80d0
StanLong <youlinbhz@sina.cn>
2022-10-08 22:06:39 +0800

    继承

--- HelloC++/通讯录管理系统.cpp
+++ HelloC++/通讯录管理系统.cpp
@@ -1,57 +1,64 @@
 # include <iostream>
 using namespace std;
 
+
+// 继承同名静态成员处理方式
 class Base
 {
     public:
-    int m_A;
+    static int m_A;
 
-    protected:
-    int m_B;
-
-    private:
-    int m_C;
-
-    public:
-    Base()
+    static void func()
     {
-        cout << "父类构造函数！" <<endl;
+        cout << "父类成员函数" << endl;
     }
-    ~Base()
+
+    static void func(int a)
     {
-        cout << "父类析构函数！" <<endl;
+        cout << "父类成员函数" << endl;
     }
 };
 
-class Son:public Base
+int Base::m_A = 100;
+
+class Son :public Base
 {
     public:
-    int m_D;
+    static int m_A;
+    static void func()
+    {
+        cout << "子类成员函数" << endl;
+    }
 
-    public:
-    Son()
-    {
-        cout << "子类构造函数！" <<endl;
-    }
-    ~Son()
-    {
-        cout << "子类析构函数！" <<endl;
-    }
 };
 
+int Son::m_A=200;
 
 void test01()
 {
     Son son; 
-    // 父类构造函数！
-    // 子类构造函数！
-    // 子类析构函数！
-    // 父类析构函数！
+    // 通过对象访问
+    cout << "访问子类同名成员，直接访问即可: " << son.m_A << endl;
+    cout << "访问父类同名成员，需要加载作用域: " << son.Base::m_A << endl;
 
-    // 根据系统输出可知
-    // 继承中的构造的析构顺序如下
-    // 先构造父类，再构造子类。先析构子类，再析构父类
 
+    // 通过类名访问
+    cout << "访问子类同名成员，直接访问即可: " << Son::m_A << endl;
+    // 第一个 :: 代表通过类名的方式访问，  第二个 :: 代表访问父类作用域下的m_A
+    cout << "访问父类同名成员，需要加载作用域: " << Son::Base::m_A << endl;
+
+    // 通过对象访问成员函数
+    son.func();
+    son.Base::func();
+
+    // 通过类名访问成员函数
+    Son::func();
+    Son::Base::func();
+
+    // 子类出现和父类同名静态成员函数，也会隐藏父类中所有同名成员函数
+    Son::Base::func(100);
+
+    
 }
 
 int main(int argc, char *argv[])
