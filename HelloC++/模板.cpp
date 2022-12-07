22e0de99f23d4df55c92d17fc2ebaf92d1cb27a3
StanLong <youlinbhz@sina.cn>
2022-12-07 21:40:10 +0800

    函数对象

--- HelloC++/模板.cpp
+++ HelloC++/模板.cpp
@@ -1,47 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<string>
+#include<vector>
+#include<ctime>
+#include<map>
 using namespace std;
-#include <list>
-#include <algorithm> // 标准算法头文件
-
-// list 容器
-void printList(const list<int> &l1) // 
+ 
+#define CEHUA 0
+#define MEISHU 1
+#define YANFA 2
+ 
+class Worker
 {
-	for(list<int>::const_iterator it = l1.begin(); it != l1.end(); it++)
+public:
+	string m_Name;
+	int m_salary;
+};
+//创建员工
+void creatWorker(vector<Worker> & v)
+{
+	string nameSeed = "ABCDEFGHIJ";
+	for (int i = 0; i < 10; i++)
 	{
-		cout << *it << " ";
+		Worker w;
+		w.m_Name = "员工";
+		w.m_Name += nameSeed[i];
+ 
+		w.m_salary = rand() % 10000 + 10000;//10000~19999
+ 
+		v.push_back(w);
 	}
-	cout << endl;
 }
-
-// 如果要降序排
-bool myCompare(int v1, int v2)
+//员工分组
+void setGroup(vector<Worker> & v,multimap<int,Worker> & m)
 {
-	return v1>v2;
+	for (vector<Worker>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		//随机产生部门编号
+		int deptId = rand() % 3;// 0 1 2
+ 
+		//将员工插入到分组中
+		//key 部门编号 value具体员工
+		m.insert(make_pair(deptId, *it));
+	}
 }
-
-void test01()
+//分组显示员工
+void showWorkerByGourp(multimap<int,Worker> & m)
 {
-	list<int> l1;
-    l1.push_back(20); 
-    l1.push_back(10);
-    l1.push_back(30);
-    l1.push_back(40);
-
-	l1.reverse(); // 反转
-
-	printList(l1);
-
-	// 排序
-	// sort(l1.begin(), l1.end()); // 所有不支持随机访问迭代器的容器，不可以用标准算法， 内部分提供对应的一些成员函数的算法
-	l1.sort();
-	printList(l1); //  10 20 30 40 , 默认升序
-	l1.sort(myCompare); // 降序排，降序排的算法自己实现
-	printList(l1);
+ 
+	//0: A B C
+	//1 :D E F 
+	//2: G ... 
+	cout << "策划部门:" << endl;
+	multimap<int,Worker>::iterator pos = m.find(CEHUA);
+	int count = m.count(CEHUA);
+	int index = 0;
+ 
+	for (;pos!=m.end() && index<count; pos++,index++)
+	{
+		cout << " 姓名: " << pos->second.m_Name << " 工资: " << pos->second.m_salary << endl;
+	}
+ 
+	cout << "------------------------" << endl;
+ 
+	cout << "美发部门:" << endl;
+	pos = m.find(MEISHU);
+	count = m.count(MEISHU);
+	index = 0;
+ 
+	for (; pos != m.end() && index<count; pos++, index++)
+	{
+		cout << " 姓名: " << pos->second.m_Name << " 工资: " << pos->second.m_salary << endl;
+	}
+ 
+	cout << "------------------------" << endl;
+ 
+	cout << "研发部门:" << endl;
+	pos = m.find(YANFA);
+	count = m.count(YANFA);
+	index = 0;
+ 
+	for (; pos != m.end() && index<count; pos++, index++)
+	{
+		cout << " 姓名: " << pos->second.m_Name << " 工资: " << pos->second.m_salary << endl;
+	}
 }
-
-
-
 int main()
 {
-    test01();
+ 
+	//随机种子
+	srand((unsigned int)time(NULL));
+ 
+	//1.创建员工
+	vector<Worker> vw;
+	creatWorker(vw);
+ 
+	//2.员工分组
+	multimap<int, Worker> mw;
+	setGroup(vw, mw);
+ 
+	//3.分组显示员工
+	showWorkerByGourp(mw);
+ 
+	//测试
+	//for (vector<Worker>::iterator it = vw.begin(); it != vw.end(); it++)
+	//{
+	//	cout << " 姓名: " << it->m_Name << " 工资: " << it->m_salary << endl;
+	//}
+ 
+	system("pause");
+ 
+	return 0;
 }
